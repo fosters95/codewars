ac9dc3d117f0caeed91242e86c4fd5b75a458110
fosters95 <fosters95@mail.ru>
2022-09-20 20:30:25 +0300

    Even or Odd

--- Js/alert.js
+++ Js/alert.js
@@ -1,7 +1,5 @@
 "use strict";
-// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-function even_or_odd(number) {
-    let rem = number % 2;
-    if (rem == 0) return 'even';
-    else return 'odd';
+// This code does not execute properly. Try to figure out why.
+function multiply($a, $b){
+    return $a * $b;
 }
