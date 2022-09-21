d3e5cb824f375bb1408a9fb0d594d62c6c65e12e
fosters95 <fosters95@mail.ru>
2022-09-21 20:05:19 +0300

    Return Negative

--- Js/alert.js
+++ Js/alert.js
@@ -1,5 +1,7 @@
 "use strict";
-// This code does not execute properly. Try to figure out why.
-function multiply($a, $b){
-    return $a * $b
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+function makeNegative(num) {
+    if (num > 0) return num * -1;
+    else return num;
 }
+
